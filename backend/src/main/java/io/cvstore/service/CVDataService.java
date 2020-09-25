1f4be817ce056dadfac850f2029c3cadf25c7fc6
Shanika Wijerathna <shanikacj@gmail.com>
2020-09-25 13:53:42 +1200

    Added integration test

--- backend/src/main/java/io/cvstore/service/CVDataService.java
+++ backend/src/main/java/io/cvstore/service/CVDataService.java
@@ -3,6 +3,7 @@ package io.cvstore.service;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import io.cvstore.domain.cv.CvData;
+import io.micronaut.context.annotation.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 
@@ -21,9 +22,15 @@ public class CVDataService {
     @Inject
     MongoClient mongoClient;
 
+    @Value("${app.mongoDb.databaseName}")
+    String databaseName;
+
+    @Value("${app.mongoDb.databaseName}")
+    String collectionName;
+
     public CvData getCvData() {
-        MongoCollection<Document> collection = mongoClient.getDatabase("cvdata")
-            .getCollection("cvdata");
+        MongoCollection<Document> collection = mongoClient.getDatabase(databaseName)
+            .getCollection(collectionName);
 
         return collection.find(CvData.class).first();
     }
--- backend/src/test/java/io/cvstore/controller/CvDataDataControllerTest.java
+++ backend/src/test/java/io/cvstore/controller/CvDataDataControllerTest.java
@@ -1,13 +1,20 @@
 package io.cvstore.controller;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import io.micronaut.context.annotation.Value;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.annotation.MicronautTest;
+import org.bson.Document;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MongoDBContainer;
 
 import javax.inject.Inject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
@@ -18,14 +25,41 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class CvDataControllerTest {
 
     @Inject
-    @Client("/data")
+    @Client("/")
     RxHttpClient client;
+
+    @Inject
+    MongoClient mongoClient;
+
+    @Value("${app.mongoDb.databaseName}")
+    String databaseName;
+
+    @Value("${app.mongoDb.databaseName}")
+    String collectionName;
+
+    final MongoDBContainer mongoDBContainer = new MongoDBContainer();
+
+    final String DATA = "{\"firstName\":\"Shanika\",\"lastName\":\"Wijerathna\"}";
+
+    @BeforeEach
+    void setUp() {
+        mongoDBContainer.start();
+        setUpData();
+    }
+
+    private void setUpData() {
+        MongoDatabase db = mongoClient.getDatabase(databaseName);
+        db.createCollection(collectionName);
+        db.getCollection(collectionName)
+            .insertOne(Document.parse(DATA));
+    }
 
     @Test
     void testGetCvData() {
-        HttpRequest<String> request = HttpRequest.GET("/");
+        HttpRequest<String> request = HttpRequest.GET("/data");
         String body = client.toBlocking().retrieve(request);
 
         assertNotNull(body);
+        assertEquals(DATA, body);
     }
 }
